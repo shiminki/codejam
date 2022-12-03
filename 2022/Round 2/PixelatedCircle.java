5cb9f98eb7b3ce94f14b0e73fc575b6d9184cc01
shiminki <77321886+shiminki@users.noreply.github.com>
2022-12-04 05:09:13 +0900

    REF: Added comments

--- 2022/Round 2/PixelatedCircle.java
+++ 2022/Round 2/PixelatedCircle.java
@@ -1,99 +1,138 @@
 import java.util.*;
 import java.io.*;
 
-public class PixelatedCircle {
+public class SpiralingIntoControl {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         PrintWriter pw = new PrintWriter(System.out);
         int T = Integer.parseInt(br.readLine());
 
         for (int t = 1; t <= T; t++) {
-            int R = Integer.parseInt(br.readLine());
+            StringTokenizer st = new StringTokenizer(br.readLine());
 
-            boolean[][] correctGrid = drawCirclesFilled(R);
-            boolean[][] wrongGrid = drawCirclesFilledWrong(R);
+            int N = Integer.parseInt(st.nextToken());
+            int K = Integer.parseInt(st.nextToken());
 
-            int ans = 0;
+            int[][] grid = spiralGrid(N); // O(N^2)
 
-            int cnt1 = 0, cnt2 = 0;
+            List<Map<Integer, Integer>> P = new ArrayList<>();
+            // P[u][k] = v indicates that one can move from 1 to u
+            // in k moves. Also, one has to visit v right before u.
 
-            for (int i = 0; i < 2 * R + 1; i++) {
-                for (int j = 0; j < 2 * R + 1; j++) {
-                    if (correctGrid[i][j] != wrongGrid[i][j]) {
-                        ans++;
-                    }
-                    if (correctGrid[i][j]) {
-                        cnt1++;
-                    }
-                    if (wrongGrid[i][j]) {
-                        cnt2++;
+            for (int i = 0; i <= N * N; i++) {
+                P.add(new HashMap<>());
+            }
+            // base case
+            P.get(1).put(0, 1);
+
+            for (int k = 0; k < K; k++) {
+                for (int x = 0; x < N; x++) {
+                    for (int y = 0; y < N; y++) {
+                        for (int neighbor : getNeighbors(x, y, N)) {
+                            int u = neighbor / N, v = neighbor % N;
+                            if (grid[u][v] > grid[x][y] && P.get(grid[x][y]).containsKey(k)) {
+                                P.get(grid[u][v]).put(k + 1, grid[x][y]);
+                            }
+                        }
                     }
                 }
             }
-            if (t == 5)
-                pw.println(String.format("%d %d", cnt1, cnt2));
 
-            /*
-             * for (int i = 0; i < correctGrid.length; i++) {
-             * pw.println(Arrays.toString(correctGrid[i]));
-             * }
-             * pw.println();
-             * for (int i = 0; i < correctGrid.length; i++) {
-             * pw.println(Arrays.toString(wrongGrid[i]));
-             * }
-             */
+            if (!P.get(N * N).containsKey(K)) {
+                pw.println(String.format("Case #%d: IMPOSSIBLE", t));
+            } else {
+                int ans = 0;
+                int num = N * N;
+                List<Integer> start = new ArrayList<>();
+                List<Integer> end = new ArrayList<>();
 
-            String output = String.format("Case #%d: %d", t, ans);
-            pw.println(output);
+                for (int k = K; k > 0; k--) {
+                    int parent = P.get(num).get(k);
+                    if (parent != num - 1) {
+                        ans++;
+                        start.add(parent);
+                        end.add(num);
+                    }
+                    num = parent;
+                }
+
+                String output = String.format("Case #%d: %d", t, ans);
+                pw.println(output);
+
+                for (int i = start.size() - 1; i >= 0; i--) {
+                    pw.println(String.format("%d %d", start.get(i), end.get(i)));
+                }
+            }
         }
         pw.flush();
         pw.close();
         br.close();
     }
 
-    static boolean[][] drawCirclePerimeter(boolean[][] grid, int r, int R) {
-        for (int i = 0; i < grid.length; i++) {
-            int x = getPos(i, R);
-            if (x * x <= r * r) {
-                int y = (int) Math.round(Math.sqrt(r * r - x * x));
-                int j = getIndex(y, R), k = getIndex(-1 * y, R);
-                grid[i][j] = true;
-                grid[i][k] = true;
-                grid[j][i] = true;
-                grid[k][i] = true;
-            }
+    static List<Integer> getNeighbors(int x, int y, int n) {
+        List<Integer> neighbors = new ArrayList<>();
+
+        if (y < n - 1) {
+            neighbors.add(n * x + y + 1);
         }
-        return grid;
+        if (y > 0) {
+            neighbors.add(n * x + y - 1);
+        }
+        if (x < n - 1) {
+            neighbors.add(n * (x + 1) + y);
+        }
+        if (x > 0) {
+            neighbors.add(n * (x - 1) + y);
+        }
+        return neighbors;
     }
 
-    static boolean[][] drawCirclesFilledWrong(int R) {
-        boolean[][] grid = new boolean[2 * R + 1][2 * R + 1];
-        for (int r = 0; r <= R; r++) {
-            grid = drawCirclePerimeter(grid, r, R);
-        }
-        return grid;
-    }
+    static int[][] spiralGrid(int N) {
+        int[][] grid = new int[N][N];
+        grid[0][0] = 0;
+        int num = 1;
+        int current_x = 0, current_y = 0;
+        int past_x = 0, past_y = -1;
 
-    static boolean[][] drawCirclesFilled(int R) {
-        boolean[][] grid = new boolean[2 * R + 1][2 * R + 1];
-
-        for (int i = 0; i < grid.length; i++) {
-            for (int j = 0; j < grid.length; j++) {
-                int x = getPos(i, R), y = getPos(j, R);
-
-                if (Math.round(Math.sqrt(x * x + y * y)) <= R) {
-                    grid[i][j] = true;
+        while (num <= N * N) {
+            if (current_x >= 0 && current_x < N && current_y >= 0 && current_y < N
+                    && grid[current_x][current_y] == 0) {
+                int x = current_x + (current_x - past_x);
+                int y = current_y + (current_y - past_y);
+                past_x = current_x;
+                past_y = current_y;
+                current_x = x;
+                current_y = y;
+            } else {
+                // change direction
+                // if right, move down
+                if (past_y == current_y - 1) {
+                    current_x = past_x;
+                    current_y = past_y;
+                    current_x++;
                 }
+                // if down, move left
+                else if (past_x == current_x - 1) {
+                    current_x = past_x;
+                    current_y = past_y;
+                    current_y--;
+                }
+                // if left, move up
+                else if (past_y == current_y + 1) {
+                    current_x = past_x;
+                    current_y = past_y;
+                    current_x--;
+                }
+                // if up, move right
+                else if (past_x == current_x + 1) {
+                    current_x = past_x;
+                    current_y = past_y;
+                    current_y++;
+                }
+                num--;
             }
+            grid[past_x][past_y] = num++;
         }
         return grid;
-    }
-
-    static int getPos(int idx, int R) {
-        return idx - R;
-    }
-
-    static int getIndex(int x, int R) {
-        return x + R;
     }
 }
